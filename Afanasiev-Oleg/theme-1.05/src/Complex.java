5f029d2131e73700463ba619a57a2238829a0092
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-11-16 01:31:43 +0200

    "Complex numbers" task was implemented

--- Afanasiev-Oleg/theme-1.05/src/Complex.java
+++ Afanasiev-Oleg/theme-1.05/src/Complex.java
@@ -2,7 +2,74 @@
  * Created by oleg on 11/16/15.
  */
 public class Complex {
-    public Complex() {
+    private final double re;
+    private final double im;
 
+    public Complex() {
+        this(0, 0);
+    }
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double re() {
+        return re;
+    }
+
+    public double im() {
+        return im;
+    }
+
+    public Complex plus(Complex b) {
+        Complex a = this;
+        double re = a.re + b.re;
+        double im = a.im + b.im;
+        return new Complex(re, im);
+    }
+
+    public Complex minus(Complex b) {
+        Complex a = this;
+        double re = a.re - b.re;
+        double im = a.im - b.im;
+        return new Complex(re, im);
+    }
+
+    public Complex mult(Complex b) {
+        Complex a = this;
+        double re = a.re * b.re - a.im * b.im;
+        double im = a.re * b.im + a.im * b.re;
+        return new Complex(re, im);
+    }
+
+    public Complex mult(double c) {
+        Complex a = this;
+        double re = a.re * c;
+        double im = a.im * c;
+        return new Complex(re, im);
+    }
+
+    public Complex div(Complex b) {
+        Complex a = this;
+        return a.mult(b.reciprical());
+    }
+
+    public Complex reciprical() {
+        double scale = re*re + im*im;
+        return new Complex(re/scale, -im/scale);
+    }
+
+    public String toString() {
+        if (im == 0)
+            return re + "";
+
+        if (re == 0)
+            return im + "i";
+
+        if (im < 0)
+            return re + " - " + (-im) + "i";
+
+        return re + " + " + im + "i";
     }
 }
--- Afanasiev-Oleg/theme-1.05/src/Main.java
+++ Afanasiev-Oleg/theme-1.05/src/Main.java
@@ -3,7 +3,20 @@
  */
 public class Main {
     public static void main(String[] args) {
-        Complex complex = new Complex();
-        System.out.println("hello");
+        Complex z1 = new Complex(2, 4);
+        Complex z2 = new Complex(5, 7);
+        double c = 5;
+
+        System.out.println("z1 = " + z1);
+        System.out.println("z2 = " + z2);
+        System.out.println("Re(z1) = " + z1.re());
+        System.out.println("Im(z1) = " + z1.im());
+        System.out.println("z1 + z2 = " + z1.plus(z2));
+        System.out.println("z1 - z2 = " + z1.minus(z2));
+        System.out.println("z1 * z2 = " + z1.mult(z2));
+        System.out.printf("z1 * %.2f = %s \n", c, z1.mult(c));
+        System.out.println("z1 / z2 = " + z1.div(z2));
+        System.out.println("1 / z1 = " + z1.reciprical());
+        System.out.println("(z1 / z2) * z2 = " + z1.div(z2).mult(z2));
     }
 }
