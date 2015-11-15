7e592bdb02a5deca97f7b5b68351f51e7f0e4ce6
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-11-15 23:58:39 +0200

    create dir structure

---
+++ Afanasiev-Oleg/theme-1.05/src/Complex.java
@@ -0,0 +1,8 @@
+/**
+ * Created by oleg on 11/15/15.
+ */
+public class Complex {
+    public Complex() {
+
+    }
+}
---
+++ Afanasiev-Oleg/theme-1.05/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by oleg on 11/15/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Complex complex = new Complex();
+        System.out.println("Hello!");
+    }
+}
