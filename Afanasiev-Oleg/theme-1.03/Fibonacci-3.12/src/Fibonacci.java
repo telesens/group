0d8961dccb0c7a1daaa3b6943ed6b2be274dc119
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-11-17 22:20:14 +0200

    Task calculation PI using Nilakanthas series was implemented. Two algorithm were realized: iterative and recursive.

---
+++ Afanasiev-Oleg/theme-1.03/Fibonacci-3.12/src/Fibonacci.java
@@ -0,0 +1,10 @@
+/**
+ * Created by oleg on 11/17/15.
+ */
+public class Fibonacci {
+    public Fibonacci() {
+
+    }
+
+    
+}
---
+++ Afanasiev-Oleg/theme-1.03/Fibonacci-3.12/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by oleg on 11/17/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Fibonacci");
+    }
+}
