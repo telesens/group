2962f5062e367a266298482a1976427956e4996b
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-11-21 07:58:02 +0200

    The task "f-minimum-ref-3.3" with relations on method was realized

---
+++ afanasiev-oleg/module-1.06/f-minimum-ref-3.3/src/MyFunc.java
@@ -0,0 +1,11 @@
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Created by oleg on 11/21/15.
+ */
+public class MyFunc {
+
+    public static  double f(double x) {
+        return x * Math.sin(x);
+    }
+}
