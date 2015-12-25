ec4997f13bed975a2e6fb3aab06e0f8f12834c01
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-12-25 19:00:18 +0200

    The task "exc-3.03-xml-jaxb" was realized

--- afanasiev-oleg/module-2.05/exc-3.03-xml-jaxb/src/com/telesens/afanasiev/module_2_05/Main.java
+++ afanasiev-oleg/module-2.05/exc-3.03-xml-jaxb/src/com/telesens/afanasiev/module_2_05/Main.java
@@ -10,8 +10,10 @@ import com.telesens.afanasiev.module_2_05.scheme.City;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -28,6 +30,16 @@ public class Main {
 
             outputCity(city);
 
+            modifyDistrict(city, "Журавлевка", "Журавлевка", 25000);
+            addDistrict(city, "Павлово Поле", 120000);
+            deleteDistrict(city, "ХТЗ");
+
+            outputCity(city);
+
+            Marshaller marshaller = jaxbContext.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(city, new FileWriter("./resources/cityCorrected.xml"));
+
         } catch(JAXBException | IOException exc) {
             exc.printStackTrace();
         }
@@ -40,11 +52,41 @@ public class Main {
         System.out.println("|      Район      |  Население |");
         System.out.println("+-----------------+------------+");
 
-        City.Districts districts = city.getDistricts();
-        City.Districts.District district = districts.getDistrict();
-
+        for (City.Districts.District district : city.getDistricts().getDistrict()) {
             System.out.printf("|  %-14s |   %-8s | %n",
                     district.getValue(), district.getPopulation() == null ? "" : district.getPopulation());
+        }
+        System.out.println("+-----------------+------------+");
+    }
 
+    private static void addDistrict(City city, String name, int population) {
+        City.Districts.District district = new City.Districts.District();
+        district.setValue(name);
+        district.setPopulation(population);
+
+        city.getDistricts().getDistrict().add(district);
+    }
+
+    private static void modifyDistrict(City city, String oldName, String newName, int population) {
+        for (City.Districts.District district : city.getDistricts().getDistrict()) {
+            if (district.getValue().equals(oldName)) {
+                district.setValue(newName);
+                district.setPopulation(population);
+                break;
+            }
+        }
+    }
+
+    private static void deleteDistrict(City city, String name) {
+        City.Districts.District districtTarget = null;
+
+        for (City.Districts.District district : city.getDistricts().getDistrict()) {
+            if (district.getValue().equals(name)) {
+                districtTarget = district;
+                break;
+            }
+        }
+
+        city.getDistricts().getDistrict().remove(districtTarget);
     }
 }
