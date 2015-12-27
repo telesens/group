31bab305976718ad4435c8585705d6886db295ed
Oleg Afanasiev <oleg.kh81@gmail.com>
2015-12-27 04:39:15 +0200

    The task "exc-3.02-reflection-call-func" was realized

--- afanasiev-oleg/module-2.10/exc-3.02-reflection-call-func/src/com/telesens/afanasiev/Main.java
+++ afanasiev-oleg/module-2.10/exc-3.02-reflection-call-func/src/com/telesens/afanasiev/Main.java
@@ -3,14 +3,27 @@
  */
 package com.telesens.afanasiev;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * Created by oleg on 12/26/15.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("---------------Reflection call method--------------\n");
 
-        new ClassOne().method();
-        new ClassTwo().method();
+        ClassOne classOne = new ClassOne();
+        ClassTwo classTwo = new ClassTwo();
+
+        try {
+            Method methodOne = Class.forName("com.telesens.afanasiev.ClassOne").getMethod("method");
+            Method methodTwo = Class.forName("com.telesens.afanasiev.ClassTwo").getMethod("method");
+
+            methodOne.invoke(classOne);
+            methodTwo.invoke(classTwo);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException exc) {
+            exc.printStackTrace();
+        }
     }
 }
